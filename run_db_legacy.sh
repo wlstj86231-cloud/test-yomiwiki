5efacfd3e3b64a338271bb959969cbd10a7c013c
wlstj86231 <wlstj86231@gmail.com>
2026-03-19 17:54:01 +0000

    security: Removed sensitive DB scripts and architecture docs from git tracking

--- run_db_legacy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# [LEGACY MODE] - 기존 토큰 및 'yomi-db-prod' DB 사용
-export CLOUDFLARE_API_TOKEN="cfut_V63V7wKw8G8zLhH3l9KjZawx0QUPiUoFNLMSQH2Ceb24d2c1"
-export CLOUDFLARE_ACCOUNT_ID="e56e1153c80086f2470940d664b46eb3"
-
-echo "[SYSTEM]: Running in LEGACY mode..."
-npx wrangler d1 execute yomi-db-prod --remote --file=./schema.sql -y
