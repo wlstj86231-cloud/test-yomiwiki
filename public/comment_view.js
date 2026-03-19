424548297c997b72134f9d4cde45c48d50ac9c7f
wlstj86231 <wlstj86231@gmail.com>
2026-03-19 13:31:40 +0000

    feat: v1.5.4 - complete Item 22 (create CommentView component file)

---
+++ public/comment_view.js
@@ -0,0 +1,14 @@
+/**
+ * CommentView Component (Item 22)
+ * This file will handle the rendering and logic for article-specific discussions.
+ */
+
+window.CommentView = {
+    /**
+     * Renders the comment section placeholder.
+     * Logic for fetching and displaying comments will be added in subsequent steps.
+     */
+    init: function(articleId) {
+        // To be implemented
+    }
+};
